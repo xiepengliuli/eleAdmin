ec7ac8cae03afe8d7ce4422ba619f2fd3065596a
xiepengtest <xiepengtest@163.com>
2017-06-08 20:18:25 +0800

    富文本上传

--- src/commponents/testa.vue
+++ src/commponents/testa.vue
@@ -1,32 +1,36 @@
 <template>
-<el-row>
-  <el-col :span="24">
-  bbb
-	<ueditor></ueditor>
-  ccc
-</el-col>
-</el-row>
-	
+  <el-upload style="width: 800px;"
+  class="upload-demo"
+  action="http://localhost:8080/ueditor1_4_3_3-utf8-jsp/jsp/controller.jsp?action=uploadfile"
+  :on-preview="handlePreview"
+  :on-remove="handleRemove"
+  :file-list="fileList">
+  <el-button size="small" type="primary">点击上传</el-button>
+  <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
+</el-upload>
 </template>
-
 <script>
-
-import Ueditor from "../assets/ueditor/ueditor.vue"
-  
   export default {
-   components: {
-      // <my-component> 将只在父模板可用
-      'ueditor': Ueditor
-    },
-  	mounted:function(){
-  	},
     data() {
       return {
-        
+        fileList: [{
+          name: 'food.jpeg',
+          url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100',
+          status: 'finished'
+        },{
+          name: 'food2.jpeg',
+          url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100',
+          status: 'finished'
+        }]
       };
     },
     methods: {
-      
+      handleRemove(file, fileList) {
+        console.log(file, fileList);
+      },
+      handlePreview(file) {
+        console.log(file);
+      }
     }
-  };
+  }
 </script>
